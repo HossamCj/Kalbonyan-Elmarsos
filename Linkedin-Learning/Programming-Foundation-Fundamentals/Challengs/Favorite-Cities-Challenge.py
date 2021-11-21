ea327032f39714b6c8a5977db89b772399be64e7
Hossam Cj <hossamcj@gmail.com>
2021-11-21 21:15:30 +0200

    Add favorite-cities-challenge in 2 different ways

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,25 @@
+# Has a function named favorite_city
+# favorite_city should have one parameter: name
+# You should call favorite city at least three times
+# The output should include : "One of my favorite cities is"
 
+def favorite_city(name):
+    output_message = "One of my favorite cities is: " + name
+    return output_message
+
+cairo = favorite_city('Cairo')
+alex = favorite_city('Alex')
+el_madina = favorite_city('El Madina')
+
+print(cairo)
+print(alex)
+print(el_madina)
+
+# another way for making look like the instructor solution 
+
+def my_favorite_city(name):
+    print('One of my favorite cities is ', name)
+    
+my_favorite_city('Shatby beach, Alexandria')
+my_favorite_city('Dar El Salam Library, Cairo')
+my_favorite_city('Connecticut, In USA')
