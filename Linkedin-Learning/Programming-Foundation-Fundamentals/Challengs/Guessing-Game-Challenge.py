8a7d1399f131735abc9878a6803af1b40aa71732
Hossam Cj <hossamcj@gmail.com>
2021-11-21 16:23:52 +0200

    Add Gussing game challenge in Programming Foundations: Fundamentals course

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,11 @@
 
+question1 = input("What is your favorite food? ")
+
+my_favorite_food = "Chickens"
+
+if question1 == my_favorite_food:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+    
+print('Thanks for the playing!')
