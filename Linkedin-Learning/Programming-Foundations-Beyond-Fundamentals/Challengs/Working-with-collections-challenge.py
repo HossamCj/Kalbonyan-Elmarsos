2d7069a4057129a33cc2610065a5d36dccc9499d
Hossam Cj <hossamcj@gmail.com>
2021-11-22 13:54:51 +0200

    Solve  collections challenge, Beyond-Fundamentals

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,42 @@
+# Nearest starts to the Earth
+# star1 = 'Sol'
+# start2 = 'Alpha Centauri'
+# start3 = 'Barnard'
+# start3 = 'Wolf 359'
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+print(stars[3])
+
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_America = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+
+peaks = {
+    'African': 'Kilimanjro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_America': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_America': 'Aconcagua',
+}
+
+print(peaks['Pacific'])
+
+
+
+
 
