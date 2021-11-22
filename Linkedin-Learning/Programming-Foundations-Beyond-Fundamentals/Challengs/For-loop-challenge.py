03e762a0011e3fbfd5c14bf70bcecb6e13607483
Hossam Cj <hossamcj@gmail.com>
2021-11-22 17:01:08 +0200

    Add the main for loop challenge with the extra challenge

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,25 @@
+# The main Challenge 
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
 
+print('Our fruit selection: ')
+
+for fruit in fruits:
+    print(fruit)
+    
+    
+# Extra Challenge that the instructor offer: 
+numbers = [1, 2, 3, 4, 5]
+
+calculation_resultes = 0
+
+for number in numbers:
+    calculation_resultes += number
+    
+print(calculation_resultes)
