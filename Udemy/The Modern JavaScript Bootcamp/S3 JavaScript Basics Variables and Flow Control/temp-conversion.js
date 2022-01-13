90feea10d7eb114fcfcb30b5f2ee259191b79267
Hossam Cj <hossamcj@gmail.com>
2022-01-13 23:41:54 +0200

    js-course, Add temp-conversion challange

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,13 @@
-The Modern JavaScript Bootcamp
+// Calculate celsius and store in celsius variable
+let fahrenheit = 45
+let calsius = (fahrenheit - 32) * 5 / 9
+
+// Print that value
+console.log(calsius)
+
+// Calculate kelvin value and store  in variable
+let kelvin = calsius + 273.15
+
+// Print that value
+console.log(kelvin)
+
