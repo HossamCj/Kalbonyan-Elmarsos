ffd6a4b4ed7cd182d98c331e66be69898d728192
Hossam Cj <hossamcj@gmail.com>
2022-01-13 14:41:43 +0200

    js-course, add strings challenge

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
@@ -1,1 +1,9 @@
-The Modern JavaScript Bootcamp
+// Challenge 1
+// City, Country, Location
+
+// In this challenge I was testing the "Var keyword for the variables and the ;"
+var city = 'Giza'
+var country = 'Egypt';
+let location = city + ', ' + country;
+
+console.log(location)
