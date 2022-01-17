03b0e2f31a973333ff9eb1fb6183d74a76b88de3
Hossam Cj <hossamcj@gmail.com>
2022-01-17 22:56:13 +0200

    js-course, Add scope exercises

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope-part2.js
@@ -1,1 +1,19 @@
-The Modern JavaScript Bootcamp
+// Global (name)
+    // Local (name)
+        // Local
+    // Local
+
+let name = 'Hossam'
+
+if (true) {
+    let name = 'Abo Safia'
+
+    console.log(name)
+    
+    if (true) {
+        name = 'Abo Noo7'
+    }
+    
+    console.log(name)
+}
+
--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/scope.js
@@ -1,1 +1,17 @@
-The Modern JavaScript Bootcamp
+let varOne = 'Var one'
+
+if (true) {
+    console.log(varOne)
+
+    let varTwo= 'Var two'
+
+    if (true) {
+        console.log(varTwo)
+    }
+}
+
+if (true) {
+    let varThree = 'Var three'
+}
+
+// console.log(varTwo)
