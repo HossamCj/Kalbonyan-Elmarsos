3c9e1ca0f150fc0cd12180239de092d1fae5ab23
Hossam Cj <hossamcj@gmail.com>
2022-01-18 14:39:12 +0200

    Correct Celsius word instead of calsius

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,12 +1,12 @@
 // Calculate celsius and store in celsius variable
 let fahrenheit = 45
-let calsius = (fahrenheit - 32) * 5 / 9
+let celsius = (fahrenheit - 32) * 5 / 9
 
 // Print that value
-console.log(calsius)
+console.log(celsius)
 
 // Calculate kelvin value and store  in variable
-let kelvin = calsius + 273.15
+let kelvin = celsius + 273.15
 
 // Print that value
 console.log(kelvin)
