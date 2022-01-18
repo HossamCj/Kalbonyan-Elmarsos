0543eb752c23f172a4381c5e9b6104098b085406
Hossam Cj <hossamcj@gmail.com>
2022-01-18 16:40:33 +0200

    js-course, Add functions challenge

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
@@ -1,1 +1,15 @@
-The Modern JavaScript Bootcamp
+// Challenge area
+
+// Convert fahrenheit to celsius
+let calsiusConvertor = function (fahrTemp) {
+    let convertor = (fahrTemp - 32) * 5 / 9
+    return convertor
+}
+
+// Call a couple of times (32 -> 0) (68 -> 20
+let _32_value = calsiusConvertor(32)
+let _68_value = calsiusConvertor(68)
+
+// Print the converted values
+console.log(_32_value)
+console.log(_68_value)
