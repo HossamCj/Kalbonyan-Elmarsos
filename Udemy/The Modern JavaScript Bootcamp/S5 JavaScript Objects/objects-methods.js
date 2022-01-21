9aa4660f24802943c63d41d1166930663ab1fca7
Hossam Cj <hossamcj@gmail.com>
2022-01-21 17:05:54 +0200

    js-course, Add objects-methods add guest challenge

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-methods.js
@@ -1,1 +1,35 @@
-The Modern JavaScript Bootcamp
+let restaurant = {
+    name: 'Atib Shamy',
+    guestCapacity: 20,
+    guestCount: 0,
+
+    checkAvailability: function (groupCount) {
+        let seatsLeft = this.guestCapacity - this.guestCount
+        return groupCount <= seatsLeft
+    },
+
+    addGuest: function (guestSize) {
+        let seatsLeft = this.guestCapacity - this.guestCount
+        if (guestSize <= seatsLeft) {
+            this.guestCount += guestSize
+        } else {
+            console.log('There\'s no available seats for your group, sorry!')
+        }
+    },
+
+    removeGuest: function (guestSize) {
+        this.guestCount -= guestSize
+    }
+}
+
+
+// Challenge area
+
+// Add Group of people
+restaurant.addGuest(19)
+console.log(restaurant.guestCount)
+
+restaurant.addGuest(5)
+console.log(restaurant.guestCount)
+
+// Remove Group of people
