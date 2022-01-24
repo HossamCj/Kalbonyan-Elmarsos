0d3e53cb4f0912a5c7dafed9f2e1bd9ed51dbdfc
Hossam Cj <hossamcj@gmail.com>
2022-01-24 16:49:21 +0200

    js-course, Add todo challenge in my way not following the instructor

--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/notes.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/notes.js
@@ -1,1 +1,7 @@
-The Modern JavaScript Bootcamp
+let notesAr = ['أ', 'ب', 'ت', 'ث']
+let text = 'مجرد بعض الكمات'
+
+console.log(notesAr.length)
+console.log(Array.isArray(text))
+
+console.log(notesAr[notesAr.length -1]) // Printing the last item in the array 
--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
@@ -1,1 +1,23 @@
-The Modern JavaScript Bootcamp
+// Create an array with five todos
+// You have x Todos
+// Print the first and the second to the last items
+// ==> Todo: Read 5 pages
+
+
+let todoList = []
+
+let todoApp = function (todo) {
+    todoList.push(todo)
+    let popupMessage = `You have: ${todoList.length} todos, `
+
+    return popupMessage + todoList[todoList.length - 1]
+}
+
+console.log(todoApp('Read 20 pages'))
+console.log(todoApp('30 Pushup'))
+console.log(todoApp('Read 1 page with the family'))
+console.log(todoApp(' Do 5 pullup'))
+console.log(todoApp(' Do 4 pullup'))
+
+console.log(todoList)
+
