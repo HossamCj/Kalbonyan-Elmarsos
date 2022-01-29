2d59f1feffdcb30bd61ba0b2777e25c62f0163f8
Hossam Cj <hossamcj@gmail.com>
2022-01-29 22:47:26 +0200

    Add todo-app challenge, Adding elements via DOM

--- Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-app.js
+++ Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-app.js
@@ -8,6 +8,11 @@ p.remove()
 const ps = document.querySelectorAll('p')
 
 ps.forEach(function (p) {
-    p.textContent = 'New value from DOM'
+    p.textContent = 'Replacing a new value from js'
     // p.remove()
 })
+
+// Create new element 
+const newH2 = document.createElement('h2')
+newH2.textContent = 'New Heading from js'
+document.querySelector('body').appendChild(newH2)
--- Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/todo-app/todo-app.js
+++ Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/todo-app/todo-app.js
@@ -1,7 +1,31 @@
-const todosToDelete = document.querySelectorAll('li')
+const todoList = [{
+    text: 'Three hours 30 minutes of coding on js-udemy',
+    completed: false
+}, {
+    text: 'Read ( Zad Werd )',
+    completed: false
+}, {
+    text: 'Read 20 pages from time management book',
+    completed: false
+}, {
+    text: 'Visit the family',
+    completed: true
+}]
 
-todosToDelete.forEach(function (todo) {
-    if (todo.textContent.toLowerCase().includes('shehada') ) {
-        todo.remove()
-    }
+
+// Message like: You have 2 todos left (p element)
+// Add a p for each todo above (user text value)
+
+const inCompletedTodo = todoList.filter(function (todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h3')
+summary.textContent = `You have ${inCompletedTodo.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todoList.forEach(function (todo) {
+    const li = document.createElement('li')
+    li.textContent = todo.text
+    document.querySelector('ol').appendChild(li)
 })
