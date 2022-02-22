e7ff6a3db4957c5adcd57897b793af93fb0396c0
Hossam Cj <hossamcj@gmail.com>
2022-02-23 01:07:41 +0200

    Add moment library srcs

--- Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-app.js
+++ Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-app.js
@@ -1,18 +1,22 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
 renderNotes(notes, filters)
 
 document.querySelector('#add-note').addEventListener('click', function (e) {
     const id = uuidv4()
+    const timesTamp = moment().valueOf()
 
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timesTamp,
+        updatedAt: timesTamp,
     })
     
     saveNotes(notes)
@@ -26,10 +30,24 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 })  
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
+    filters.sortBy = e.target.value
+    renderNotes(notes, filters)
 })
 
 window.addEventListener('storage', function (e) {
     notes = JSON.parse(e.newValue)
     renderNotes(notes, filters)
 })
+
+// const now = moment()
+// now.add(3, 'year').subtract(20, 'year')
+
+// console.log(now.format('MMMM Do, YYYY'))
+
+// const nowTimestamp = now.valueOf()
+// console.log(moment(nowTimestamp).toString())
+
+const birthday = moment()
+birthday.year(1997).month(2).date(9)
+
+console.log(birthday.format('MMM D, YYYY'))
--- Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-edit.js
+++ Udemy/The Modern JavaScript Bootcamp/S7 Javascript in the Browser/notes-app/notes-edit.js
@@ -1,6 +1,7 @@
 const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
+const lastEdited = document.querySelector('#last-edited')
 const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
 
@@ -12,18 +13,25 @@ if (note === undefined) {
     location.assign('index.html')
 }
 
+
 titleElement.value = note.title
 bodyElement.value = note.body
+lastEdited.textContent = generateLastEdited(note.updatedAt)
+
 
 // Save title edits
 titleElement.addEventListener('input', function (e) {
     note.title = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
     saveNotes(notes)
 })
 
 // Save body edits
 bodyElement.addEventListener('input', function (e) {
     note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
     saveNotes(notes)
 })
 
@@ -49,7 +57,7 @@ window.addEventListener('storage', function (e) {
 
         titleElement.value = note.title
         bodyElement.value = note.body
-        
+        lastEdited.textContent = generateLastEdited(note.updatedAt)
     }
 
 
