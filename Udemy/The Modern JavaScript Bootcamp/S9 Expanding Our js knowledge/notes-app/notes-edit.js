b2d92154ed77c0d52f7f334398355134c07a49ca
Hossam Cj <hossamcj@gmail.com>
2022-03-10 15:13:32 +0200

    Add try-catch methods to gradeCalc app

--- Udemy/The Modern JavaScript Bootcamp/S9 Expanding Our js knowledge/notes-app/notes-edit.js
+++ Udemy/The Modern JavaScript Bootcamp/S9 Expanding Our js knowledge/notes-app/notes-edit.js
@@ -45,9 +45,7 @@ window.addEventListener('storage', (e) => {
     if (e.key === 'notes') {
         notes = JSON.parse(e.newValue)
 
-        note = notes.find(function (note) {
-            return note.id === noteId
-        })
+        note = notes.find((note) => note.id === noteId)
         
         if (!note) {
             location.assign('index.html')
