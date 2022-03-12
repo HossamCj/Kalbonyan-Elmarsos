f9c571fc25385969e10f30220285139b218ca6e4
Hossam Cj <hossamcj@gmail.com>
2022-03-13 00:56:21 +0200

    Add Hangman game base structure

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/hangman.js
@@ -1,1 +1,25 @@
-The Modern JavaScript Bootcamp
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['d', 'o', 'b', 'k', 'g']
+    
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle = puzzle + '*'
+        }
+    })
+
+    return puzzle
+}
+
+const game1 = new Hangman('Dog', 2)
+console.log(game1.getPuzzle())
+
+const game2 = new Hangman('Book', 6)
+console.log(game2.getPuzzle())
