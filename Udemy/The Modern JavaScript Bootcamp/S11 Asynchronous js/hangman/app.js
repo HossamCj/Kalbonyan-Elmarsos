dde07f4eec0cf1650fb19218d14665d6a5a66bfd
Hossam Cj <hossamcj@gmail.com>
2022-05-09 18:44:23 +0200

    Add Callback challenge

--- Udemy/The Modern JavaScript Bootcamp/S11 Asynchronous js/hangman/app.js
+++ Udemy/The Modern JavaScript Bootcamp/S11 Asynchronous js/hangman/app.js
@@ -12,7 +12,7 @@ window.addEventListener('keypress', function (e) {
     guessesElement.textContent = game1.statusMessage
 })  
 
-getPuzzle((error, puzzle) => {
+getPuzzle(5, (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -20,33 +20,10 @@ getPuzzle((error, puzzle) => {
     }
 })
 
-// Making an HTTP request
-// const request = new XMLHttpRequest()
-
-// request.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         console.log(data)
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has taken place')
-//     }
-// })
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-// request.send()
-
-// const countryCode = "MX"
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const country = data.find((country) => country.alpha2Code === countryCode)
-//         console.log(country.name)
-//     } else if (e.target.readyStatet === 4) {
-//         console.log('Unable to fetch data')
-//     }
-// })
-
-// countryRequest.open('GET', 'https://restcountries.com/v3.1/all')
-// countryRequest.send()
+getCountry('US', (error, country) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`Country name: ${country.name}`)
+    }
+})
