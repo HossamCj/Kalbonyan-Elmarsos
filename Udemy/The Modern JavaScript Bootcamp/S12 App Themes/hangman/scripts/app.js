1b958e7139b23e31e2916a11fbf1f5c30dd6409c
HossamCj <hossamcj@gmail.com>
2022-05-25 06:43:04 +0200

    Add the hangman project from scratch to s12

--- Udemy/The Modern JavaScript Bootcamp/S12 App Themes/hangman/scripts/app.js
+++ Udemy/The Modern JavaScript Bootcamp/S12 App Themes/hangman/scripts/app.js
@@ -1,1 +1,24 @@
-The Modern JavaScript Bootcamp
+const puzzleElement = document.querySelector('#puzzle')
+const guessesElement = document.querySelector('#guesses')
+let game1
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
+    puzzleElement.textContent = game1.puzzle
+    guessesElement.textContent = game1.statusMessage
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle(2)
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
