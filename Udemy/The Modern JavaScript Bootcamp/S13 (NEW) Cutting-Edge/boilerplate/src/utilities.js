bb836c4f192b54657920cff91165960c11c9dbd5
Hossam Cj <hossamcj@gmail.com>
2022-06-23 18:58:39 +0200

    Add javascript modules exercise part II

---
+++ Udemy/The Modern JavaScript Bootcamp/S13 (NEW) Cutting-Edge/boilerplate/src/utilities.js
@@ -0,0 +1,10 @@
+// Named exports
+// Default exports
+
+const add = (a, b) => a + b
+const square = (x) => x * x
+
+const name = 'abo safia'
+
+
+export { add, name , square as default }
--- Udemy/The Modern JavaScript Bootcamp/S13 (NEW) Cutting-Edge/boilerplate/webpack.config.js
+++ Udemy/The Modern JavaScript Bootcamp/S13 (NEW) Cutting-Edge/boilerplate/webpack.config.js
@@ -1,1 +1,13 @@
-The Modern JavaScript Bootcamp
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        utilities: './src/utilities.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: '[name].bundle.js',
+        clean: true
+    }
+}
